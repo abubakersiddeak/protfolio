6ea2482d7e28e64ebcdb9fef0b1b2401a8971862
Your Name <abubakersiddeak@gmail.com>
2025-03-17 03:01:06 +0600

    ubdate3

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Hero from "./component/Hero";
 import Icon from "./component/Icon";
 
 import AboutMe from "./component/AboutMe";
-import Navbar from "./component/Navbar";
 
 export default function Home() {
   return (
